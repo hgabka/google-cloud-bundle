bec3a83be193048805c506e197330174e4abd5a0
Gabe <hgabka@gmail.com>
2023-11-29 14:39:50 +0100

    fixes

---
+++ src/Media/GoogleCloudHandler.php
@@ -0,0 +1,122 @@
+<?php
+
+namespace Hgabka\GoogleCloudBundle\Media;
+
+
+use Hgabka\MediaBundle\Entity\Media;
+use Hgabka\MediaBundle\Form\File\FileType;
+use Hgabka\MediaBundle\Helper\File\FileHandler;
+use Hgabka\MediaBundle\Helper\File\FileHelper;
+use Hgabka\MediaBundle\Helper\Media\AbstractMediaHandler;
+use Symfony\Component\DependencyInjection\Attribute\AutoconfigureTag;
+use Symfony\Component\HttpFoundation\File\File;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+
+#[AutoconfigureTag('hgabka_media.media_handler')]
+class GoogleCloudHandler  extends FileHandler
+{
+    public const TYPE = 'google_cloud';
+
+    /**
+     * Files with a blacklisted extension will be converted to txt.
+     *
+     * @var array
+     */
+    private $blacklistedExtensions = [];
+
+    /** @var int */
+    private $folderDepth;
+
+    public $mediaPath;
+
+    /**
+     * @var string
+     */
+    public $protectedMediaPath;
+
+    public function setFolderDepth(int $depth)
+    {
+        $this->folderDepth = $depth;
+    }
+
+    public function setUrlGenerator(UrlGeneratorInterface $urlGenerator)
+    {
+        $this->urlGenerator = $urlGenerator;
+
+        return $this;
+    }
+
+    /**
+     * Inject the blacklisted.
+     */
+    public function setBlacklistedExtensions(array $blacklistedExtensions)
+    {
+        $this->blacklistedExtensions = $blacklistedExtensions;
+    }
+
+    /**
+     * Inject the path used in media urls.
+     *
+     * @param string $mediaPath
+     */
+    public function setMediaPath($mediaPath)
+    {
+        $this->mediaPath = $mediaPath;
+    }
+
+    /**
+     * Inject the path used in media urls.
+     *
+     * @param string $mediaPath
+     */
+    public function setProtectedMediaPath($mediaPath)
+    {
+        $this->protectedMediaPath = $mediaPath;
+    }
+
+    public function getName()
+    {
+        return 'Google Cloud Handler';
+    }
+
+    /**
+     * @return string
+     */
+    public function getType()
+    {
+        return self::TYPE;
+    }
+
+    /**
+     * @return string
+     */
+    public function getFormType()
+    {
+        return FileType::class;
+    }
+
+    /**
+     * @param mixed $object
+     *
+     * @return bool
+     */
+    public function canHandle($object): bool
+    {
+        if ($object instanceof File ||
+            ($object instanceof Media &&
+                'google_cloud' === $object->getLocation())
+        ) {
+            return true;
+        }
+
+        return false;
+    }
+
+    /**
+     * @return FileHelper
+     */
+    public function getFormHelper(Media $media)
+    {
+        return new FileHelper($media, $this->mediaPath);
+    }
+}
