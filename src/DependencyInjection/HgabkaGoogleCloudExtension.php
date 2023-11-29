5206cb6f01d738d6f840c4fe3c980aee20bd9489
hgabka <hgabka@gmail.com>
2023-11-29 14:17:55 +0100

    Update HgabkaGoogleCloudExtension.php

--- src/DependencyInjection/HgabkaGoogleCloudExtension.php
+++ src/DependencyInjection/HgabkaGoogleCloudExtension.php
@@ -12,7 +12,7 @@ use Symfony\Component\HttpKernel\DependencyInjection\Extension;
  *
  * To learn more see {@link http://symfony.com/doc/current/cookbook/bundles/extension.html}
  */
-class HgabkaSimpleContentExtension extends Extension
+class HgabkaGoogleCloudExtension extends Extension
 {
     /**
      * {@inheritdoc}
